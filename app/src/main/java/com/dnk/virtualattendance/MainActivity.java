5ee5646a9153cc52b0e449de4b3f7504b2163f2c
Deanzen Lie <deanzen.lie@binus.edu>
2024-12-07 11:45:20 +0700

    Sprint1-InitiatingLayout

--- app/src/main/java/com/dnk/virtualattendance/MainActivity.java
+++ app/src/main/java/com/dnk/virtualattendance/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dnk.virtualattendance;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText mainEmailET = findViewById(R.id.mainEmailET);
+        EditText mainPasswordET = findViewById(R.id.mainPasswordET);
+        Button mainLoginBtn = findViewById(R.id.mainLoginBtn);
+
+        // Check dan Ambil Role Usernya dari input Email dan Password
+        // Role = ?
+
+        mainLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Kalau Role nya Admin Intent ke UserSetting
+                // Kalau Role nya Staff Intent ke AttendanceMachine
+                Intent loginIntent = new Intent(MainActivity.this, UserSetting.class);
+                startActivity(loginIntent);
+            }
+        });
+
+
     }
 }
