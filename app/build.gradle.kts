c3240d3abe481605b437c854b6135be4bb88367e
itzKv <kenkenku6@gmail.com>
2024-12-20 22:58:11 +0700

    Material Calendar View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,14 +50,15 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore:24.5.0")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.libraries.places:places:3.2.0")
     implementation("com.github.prolificinteractive:material-calendarview:2.0.0")
+    implementation("com.jakewharton.threetenabp:threetenabp:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
     implementation ("com.android.support:support-compat:27.0.2")
     implementation ("androidx.core:core:1.13.0")
 }
